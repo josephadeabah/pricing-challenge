a3ae37708ab509eefc48dee6a30d509a6a516f99
nx-joseph-adeabah <joseph.adeabah@amalitech.org>
2024-09-23 07:41:56 +0000

    fix: few alignment

--- app/page.js
+++ app/page.js
@@ -34,20 +34,20 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col items-center h-full px-4 gap-8">
+    <div className="flex flex-col items-center h-full gap-8">
       <div className="w-full max-w-[392px] bg-[url('./images/pattern-circles.svg')] py-8 mt-12 bg-no-repeat bg-contain bg-center mx-auto">
         <h1 className="text-center text-2xl font-bold relative z-10">Simple, traffic-based pricing</h1>
         <h2 className="text-center text-sm opacity-60 mt-2 relative z-10 text-gray-500">Sign-up for our 30-day trial. No credit card required.</h2>
       </div>
 
-      <div className="relative bg-white rounded-lg mx-auto h-fit max-h-lg w-full max-w-md px-4 py-4 mt-10 shadow-xl">
+      <div className="relative bg-white rounded-lg mx-auto h-fit max-h-lg w-full max-w-md py-4 mt-10 shadow-xl">
         <div className="flex flex-col items-center">
           {/* Centered above the slider on mobile */}
           <p className="font-semibold text-sm opacity-60 mt-2 md:hidden text-center">{sliderValue}K Pageviews</p>
           {/* For desktop layout */}
-          <div className="hidden md:flex justify-between items-baseline w-full mt-2">
+          <div className="hidden md:flex justify-between items-center w-full px-4 mt-2">
             <p className="font-semibold text-sm opacity-60 text-gray-500">{sliderValue}K Pageviews</p>
-            <p className="font-bold text-[60px] flex items-baseline">
+            <p className="font-bold text-[60px] flex items-center">
               ${getPrice()}
               <span className="text-sm font-semibold ml-1 text-gray-400">{isYearly ? '/year' : '/month'}</span>
             </p>
@@ -58,16 +58,16 @@ const Home = () => {
             min={0}
             max={1000}
             step={100}
-            className="mt-6"
+            className="mt-6 px-4"
           />
           {/* Centered below the slider on mobile */}
           <div className="flex flex-col items-center mt-2 md:hidden">
-            <p className="font-bold text-[60px] flex items-baseline">
+            <p className="font-bold text-[60px] flex items-center">
               ${getPrice()}
               <span className="text-sm font-normal text-gray-400 ml-1">{isYearly ? '/year' : '/month'}</span>
             </p>
           </div>
-          <div className="flex justify-between gap-2 items-center text-xs font-medium text-gray-400 mt-12">
+          <div className="flex justify-between gap-2 items-center text-xs font-medium px-4 text-gray-400 mt-12">
             <p className="">Monthly Billing</p>
             <Switch
               checked={isYearly}
@@ -86,7 +86,8 @@ const Home = () => {
               </span>
             </p>
           </div>
-          <div className="mt-6 flex flex-col md:flex-row w-full md:justify-between items-center">
+          <hr className='w-full border border-1 my-5 border-gray-200' />
+          <div className="mt-6 flex flex-col md:flex-row w-full md:justify-between px-4 items-center">
             <div className="mt-6 flex flex-col items-center md:items-start">
               {['Unlimited websites', '100% data ownership', 'Email reports'].map((addon, index) => (
                 <div key={index} className="flex items-center mb-2">
